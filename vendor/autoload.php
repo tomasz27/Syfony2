715d7295e82f6ea352bcdd8c7e782fb48aee1167
tarnawski <tarnawski27@gmail.com>
2015-03-13 16:10:07 +0100

    Dodanie Alertu

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite57c57d5cbfd275aea86240396782fff::getLoader();
+return ComposerAutoloaderInit47ea6097341c08dc9e2852d4f1c9a0ed::getLoader();
